625a36ef7040ac775a3f18540fe9a6d04d39b619
Sajjad Ghafarian <s.ghafarian@asax.ir>
2023-07-09 16:48:07 +0330

    init 2;

--- PaaS/Core/KubernetesConnector/views.py
+++ PaaS/Core/KubernetesConnector/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import views, generics
+from rest_framework.response import Response
+from types import SimpleNamespace
+import json
+import requests
+from rest_framework import status
+from rest_framework import permissions
+from rest_framework.views import APIView
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core.cache import cache
+from django.conf import settings
+from kubernetes import client, config
 
-# Create your views here.
+def get_data_from_cluster(self, *args, **kwargs):
+    # todo
+    return ;
