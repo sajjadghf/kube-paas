801325f36601afd401aedc139fccb8188c220716
Sajjad Ghafarian <s.ghafarian@asax.ir>
2023-07-15 07:23:36 +0330

    base init - deployment handler CRUD url structure

--- PaaS/PaaS/urls.py
+++ PaaS/PaaS/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/kubernetes/', include('Core.KubernetesConnector.urls'))
+    path('api/v1/kubernetes/', include('Core.KubernetesConnector.urls')),
+    path('api/v1/kubernetes/deployments/', include('Core.DeploymentHandler.urls')),
 ]
